1ff93169896dc14d1bdb4b7ad9914f2802400903
zainnaveed34211 <158331210+ZainNaveed34211@users.noreply.github.com>
2024-03-31 13:13:16 +0500

    Update run-docker.sh

--- run-docker.sh
+++ run-docker.sh
@@ -9,8 +9,8 @@ if [[ ! -d "$BASE_DIR/auth" ]]; then
 fi
 
 CONTAINER="zphisher"
-IMAGE="htrtech/zphisher:latest"
-IMG_MIRROR="ghcr.io/htr-tech/zphisher:latest"
+IMAGE="ZainNaveed34211/zainnaveed:latest"
+IMG_MIRROR="ghcr.io/ZainNaveed34211/zainnaveed:latest"
 MOUNT_LOCATION=${BASE_DIR}/auth
 check_container=$(docker ps --all --format "{{.Names}}")
 
@@ -18,7 +18,7 @@ if [[ ! $check_container == $CONTAINER ]]; then
 	echo "Creating new container..."
 	docker create \
 		--interactive --tty \
-		--volume ${MOUNT_LOCATION}:/zphisher/auth/ \
+		--volume ${MOUNT_LOCATION}:/zainnaveed/auth/ \
 		--network host \
 		--name "${CONTAINER}" \
 		"${IMAGE}"
@@ -26,4 +26,4 @@ fi
 
 docker start --interactive "${CONTAINER}"
 
-# docker run --rm -ti --network="host" -v ${MOUNT_LOCATION}:/zphisher/auth/ --name "$CONTAINER" "$IMAGE"
+# docker run --rm -ti --network="host" -v ${MOUNT_LOCATION}:/zainnaveed/auth/ --name "$CONTAINER" "$IMAGE"
